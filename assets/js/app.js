af3b301c267e1d605f9a6da95b337dcca9a8fcfd
chengqinglin <chengqinglin@icloud.com>
2023-03-26 01:47:34 +0800

    fix sHeight not refresh
    Signed-off-by: chengqinglin <chengqinglin@icloud.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -501,11 +501,10 @@ function router() {
     // watch scroll
     {
       var $w = $(window);
-      var $prog2 = $('.progress-indicator-2');
-      var wh = $w.height();
-      var h = $('body').height();
-      var sHeight = h - wh;
       $w.on('scroll', function () {
+        var wh = $w.height();
+        var h = $('body').height();
+        var sHeight = h - wh;
         window.requestAnimationFrame(function () {
           var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
           updateProgress(perc);
@@ -513,6 +512,7 @@ function router() {
       });
 
       function updateProgress(perc) {
+        var $prog2 = $('.progress-indicator-2');
         $prog2.css({ width: perc * 100 + '%' });
         app.save_progress && store.set('page-progress', perc);
       }
